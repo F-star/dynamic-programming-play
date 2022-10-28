53752e0a963854d65b5c043a282ac62b1647f7c3
xigua <hhbuhuiai@163.com>
2022-10-28 23:14:57 +0800

    feat: 完成0-1背包问题算法

--- src/knapsack/knapsack_01/__tests__/knapsack_01.test.ts
+++ src/knapsack/knapsack_01/__tests__/knapsack_01.test.ts
@@ -1,25 +1,43 @@
-import { knapsackJustFillUp, knapsackMaxWeight } from '../knapsack_01';
+import { knapsackJustFillUp, knapsackMaxWeight, knapsackMinCount, knapsackWays } from '../knapsack_01';
 
 describe('0-1背包问题', () => {
-  test('0-1背包问题 —— 求可装最大重量', () => {
-    const testCases: Array<[number[], number, number]> = [
+  describe('0-1背包问题——求可装最大重量', () => {
+    test.each([
       [[1, 2, 3], 5, 5],
       [[2, 3, 5], 4, 3],
-    ];
-
-    for (const c of testCases) {
-      expect(knapsackMaxWeight(c[0], c[1])).toBe(c[2]);
-    }
+    ])('knapsack(%p, %i) -> %i', (weight, w, expected) => {
+      expect(knapsackMaxWeight(weight, w)).toBe(expected);
+    });
   });
 
-  test('0-1背包问题 —— 求能否刚好装满', () => {
-    const testCases: Array<[number[], number, boolean]> = [
+  describe('0-1背包问题——求能否刚好装满', () => {
+    test.each([
       [[1, 2, 3], 5, true],
       [[2, 3, 5], 4, false],
-    ];
-
-    for (const c of testCases) {
-      expect(knapsackJustFillUp(c[0], c[1])).toBe(c[2]);
-    }
+    ])('knapsack(%p, %i) -> %p', (weight, w, expected) => {
+      expect(knapsackJustFillUp(weight, w)).toBe(expected);
+    });
   });
+
+  describe('0-1背包问题——求刚好装满的最少物品数量', () => {
+    test.each([
+      [[1, 2, 3], 5, 2],
+      [[2, 3, 5], 4, -1],
+      [[1, 1, 1, 1, 1, 3, 4], 8, 3],
+    ])('knapsack(%p, %i) -> %i', (weight, w, expected) => {
+      expect(knapsackMinCount(weight, w)).toBe(expected);
+    });
+  });
+
+  describe('0-1背包问题——装满背包总共有几种装法', () => {
+    test.each([
+      [[2, 2], 2, 2],
+      [[1, 2, 3], 5, 1],
+      [[2, 3, 5], 4, 0],
+      [[1, 1, 1, 3, 4], 4, 4],
+    ])('knapsack(%p, %i) -> %i', (weight, w, expected) => {
+      expect(knapsackWays(weight, w)).toBe(expected);
+    });
+  });
+
 });
--- src/knapsack/knapsack_01/knapsack_01.ts
+++ src/knapsack/knapsack_01/knapsack_01.ts
@@ -3,7 +3,7 @@
 /**
  * 求最大重量
  *
- * 将 n 个物品（重量用 weight 数组表示）放入背包，在不超出背包总重量 w 的情况下，求能装入的最大重量。
+ * 将 n 个物品（重量用 weight 数组表示）装入背包，在不超出背包总重量 w 的情况下，求能装入的最大重量。
  * @param weight 各个物品的重量
  * @param w 背包可以装的最大总重量
  * @returns 能装入的最大重量
@@ -16,8 +16,8 @@ export function knapsackMaxWeight(weight: number[], w: number): number {
     dp[i] = new Array(w + 1);
   }
   // 第 0 次决策的状态先初始化好，这样才能递推
-  dp[0][0] = true; // 不放入
-  if (weight[0] <= w) { // 放入 weight[0]，当然不能越界。
+  dp[0][0] = true; // 不装入
+  if (weight[0] <= w) { // 装入 weight[0]，当然不能越界。
     dp[0][weight[0]] = true;
   }
 
@@ -41,7 +41,7 @@ export function knapsackMaxWeight(weight: number[], w: number): number {
 /**
  * 求能否刚好装满
  *
- * 将 n 个物品（重量用 weight 数组表示）放入背包，背包容量为 w，能否刚好装满背包？
+ * 将 n 个物品（重量用 weight 数组表示）装入背包，背包容量为 w，能否刚好装满背包？
  * @param weight 各个物品的重量
  * @param w 背包可以装的最大总重量
  * @returns 能装入的最大重量
@@ -54,8 +54,8 @@ export function knapsackJustFillUp(weight: number[], w: number): boolean {
     dp[i] = new Array(w + 1).fill(false);
   }
   // 第 0 次决策的状态先初始化好，这样才能递推
-  dp[0][0] = true; // 不放入
-  if (weight[0] <= w) { // 放入 weight[0]，当然不能越界。
+  dp[0][0] = true; // 不装入
+  if (weight[0] <= w) { // 装入 weight[0]，当然不能越界。
     dp[0][weight[0]] = true;
   }
 
@@ -66,6 +66,73 @@ export function knapsackJustFillUp(weight: number[], w: number): boolean {
         (j - weight[i] >= 0 && dp[i - 1][j - weight[i]] === true)
       ) {
         dp[i][j] = true;
+      }
+    }
+  }
+
+  return dp[n - 1][w];
+}
+
+/**
+ * 刚好装满书包总最少物品数
+ *
+ * 将 n 个物品（重量用 weight 数组表示）装入背包，背包容量为 w，求装刚好满背包的最少物品数。
+ * @param weight 各个物品的重量
+ * @param w 背包可以装的最大总重量
+ * @returns 最少物品数
+ */
+export function knapsackMinCount(weight: number[], w: number): number {
+  const n = weight.length;
+  // 初始化 number 类型的二维数组，范围：[n][w + 1]
+  const dp: number[][] = new Array(n);
+  for (let i = 0; i < n; i++) {
+    dp[i] = new Array(w + 1).fill(Number.MAX_SAFE_INTEGER);
+  }
+  // 第 0 次决策的状态先初始化好，这样才能递推
+  dp[0][0] = 0; // 不装入
+  if (weight[0] <= w) { // 装入 weight[0]，当然不能越界。
+    dp[0][weight[0]] = 1; // 装入第一个
+  }
+
+  for (let i = 1; i < n; i++) {
+    for (let j = 0; j <= w; j++) {
+      dp[i][j] = dp[i - 1][j]; // 不装入
+      if (j - weight[i] >= 0 && dp[i - 1][j - weight[i]] !== Number.MAX_SAFE_INTEGER) {
+        dp[i][j] = Math.min(dp[i - 1][j], dp[i - 1][j - weight[i]] + 1); // 装入
+      }
+    }
+  }
+
+  if (dp[n - 1][w] === Number.MAX_SAFE_INTEGER) return -1;
+  return dp[n - 1][w];
+}
+
+/**
+ * 装满背包总共有几种装法
+ *
+ * 将 n 个物品（重量用 weight 数组表示）装入背包，背包容量为 w，求装满背包总共有几种装法。
+ * @param weight 各个物品的重量
+ * @param w 背包可以装的最大总重量
+ * @returns 几种装法
+ */
+export function knapsackWays(weight: number[], w: number): number {
+  const n = weight.length;
+  // 初始化 number 类型的二维数组，范围：[n][w + 1]
+  const dp: number[][] = new Array(n);
+  for (let i = 0; i < n; i++) {
+    dp[i] = new Array(w + 1).fill(0);
+  }
+  // 第 0 次决策的状态先初始化好，这样才能递推
+  dp[0][0] = 1; // 不装入
+  if (weight[0] <= w) { // 装入 weight[0]，当然不能越界。
+    dp[0][weight[0]] = 1; // 装入第一个
+  }
+
+  for (let i = 1; i < n; i++) {
+    for (let j = 0; j <= w; j++) {
+      dp[i][j] = dp[i - 1][j];
+      if (j - weight[i] >= 0) {
+        dp[i][j] += dp[i - 1][j - weight[i]];
       }
     }
   }
